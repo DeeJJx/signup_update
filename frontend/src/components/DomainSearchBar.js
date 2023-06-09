20d329b5e00fbfb87eeeffd1e4af7d1b1936eca0
DeeJJx <james.johnson@graduate.org>
2023-06-09 07:21:53 +0100

    added api call to DomainSearchBar to check availability

--- frontend/src/components/DomainSearchBar.js
+++ frontend/src/components/DomainSearchBar.js
@@ -3,18 +3,39 @@ import React, { useState } from "react";
 
 const DomainSearchBar = () => {
     const [domainName, setDomainName] = useState('');
+    const [searchResult, setSearchResult] = useState(false);
 
     const handleInputChange = (e) => {
         setDomainName(e.target.value);
     }
 
+
+
     async function handleSubmit(e) {
         e.preventDefault();
+
+        const sanitizedDomain = domainName.replace(/^(https?:\/\/)?(www\.)?/i, '');
+
         // const whois = require('whois-json');
+        const url = `https://domaination.p.rapidapi.com/domains/${sanitizedDomain}`;
+        // const url = domainName;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '9951feb9f2msha1a48e515443097p1872dcjsn25903bbc04dc',
+                'X-RapidAPI-Host': 'domaination.p.rapidapi.com'
+            }
+        };
 
         if(domainName){
-            // var results = await whois(domainName);
-            // console.log(JSON.stringify(results, null, 2));
+            try {
+                const response = await fetch(url, options);
+                const result = await response.json();
+                console.log(result);
+                setSearchResult(result);
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 
@@ -29,6 +50,7 @@ const DomainSearchBar = () => {
                 />
                 <button type="submit">Search</button>
             </form>
+            {searchResult && <div>{searchResult.domain.isAvailable ? (`Congrats Your domain ${searchResult.domain.name} is available`) : (`Sorry Your domain ${searchResult.domain.name} is already taken`)}</div>}
         </div>
     )
 }
