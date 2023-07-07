eb25138e384f60037d9115eeff2cbc35dca8b34e
DanielT94 <daniel.turnbull94@gmail.com>
2023-07-07 11:04:55 +0100

    Added code to close hamburger menu when clicking away

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,50 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 import Hamburger from './Hamburger';
-import React, { useState } from "react";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const hamburgerRef = useRef(null);
 
-    const {logout} = useLogout();
-    const { user } = useAuthContext();
-    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
 
-    const toggleHamburger = () =>{
-        setHamburgerOpen(!hamburgerOpen)
-    }
+  const handleClick = () => {
+    logout();
+  };
 
-    const handleClick = () => {
-        logout();
-    }
-    return (
-        <header className="navbar">
-            <div className="logo">
-                <p>Twenny</p>
-            </div>
-            <div className="navbar-links">
-                <Link to="/home" className="home">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/projects">Projects</Link>
-            </div>
-            <div className="contact-us">
-                <Link to="/contactus">Contact Us</Link>    
-            </div>
-            <div className={`hamburger ${hamburgerOpen ? 'open' : ''}`} onClick={toggleHamburger}>
-                <Hamburger />
-            </div>
-            {hamburgerOpen && (
-                <div className="hamburger-links">
-                <Link to="/home" className="home">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contactus">Contact Us</Link>
-                </div>
-            )}
-        </header>
-    )
-}
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (hamburgerRef.current && !hamburgerRef.current.contains(event.target)) {
+        setHamburgerOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header className="navbar">
+      <div className="logo">
+        <p>Twenny</p>
+      </div>
+      <div className="navbar-links">
+        <Link to="/home" className="home">
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/projects">Projects</Link>
+      </div>
+      <div className="contact-us">
+        <Link to="/contactus">Contact Us</Link>
+      </div>
+      <div className={`hamburger ${hamburgerOpen ? 'open' : ''}`} ref={hamburgerRef} onClick={toggleHamburger}>
+        <Hamburger />
+      </div>
+      {hamburgerOpen && (
+        <div className="hamburger-links">
+          <Link to="/home" className="home">
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contactus">Contact Us</Link>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Navbar;
