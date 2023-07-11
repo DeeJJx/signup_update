360485f651f685dc7916cae181b1992ea41ad8cd
DanielT94 <daniel.turnbull94@gmail.com>
2023-07-11 17:47:11 +0100

    Added dropdown menus under footer titles for mobile

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,37 +1,87 @@
 import { Link } from 'react-router-dom';
+import {useState} from 'react'
 
 const Footer = () => {
 
     const year = new Date().getFullYear();
-  
-    return (
+    const [isActive, setisActive] = useState(Array(4).fill(false));
+
+    const handleClick = (index) => {
+        setisActive((prevState) => {
+          const updatedState = [...prevState];
+          updatedState[index] = !prevState[index];
+          return updatedState;
+        });
+      };
+
+      return (
         <div className="footer">
-            <div className="footer-links">
-                <div className="logo">Twenny</div>
-                <div className="tos">
-                    <div className="title">Terms and Policies</div>
-                    <Link to="/terms" className="terms">Terms of Service</Link>
-                    <Link to="/privacy" classNamer="privacy">Privacy Policy</Link>
-                </div>
-                <div className="company">
-                    <div className="title">Company</div>
-                    <Link to="/home" className="home">Home</Link>
-                    <Link to="/aboutus" className="about">About Us</Link>
-                    <Link to="/contactus" className="contactus">Contact Us</Link>
-                </div>
-                <div className="contact">
-                    <div className="title">Contact</div>
-                    <p>(+44)749 645 4598</p>
-                    <p>twenny@gmail.com</p>
-                </div>
-                <div class Name="location">
-                    <div className="title">Location</div>
-                    <p>London | Edinburgh</p>
-                    <p>Streatham | Newhaven</p>
-                    <p>DL3 BDE</p>
-                    <p>team@twenny.org</p>
-                </div>
+          <div className="footer-links">
+            <div className="logo">Twenny</div>
+            <div className="tos">
+              <div className="title" onClick={() => handleClick(0)}>
+                Terms and Policies
+                {isActive[0] && (
+                    <span className="mobile-icon">&times;</span> // Cross icon when open (visible in mobile)
+                )}
+                {!isActive[0] && (
+                    <span className="mobile-icon">&#9660;</span> // Right arrow when closed (visible in mobile)
+                )}
+              </div>
+              <div className={isActive[0] ? '' : 'hide'}>
+                <Link to="/terms" className="terms">Terms of Service</Link>
+                <Link to="/privacy" className="privacy">Privacy Policy</Link>
+              </div>
             </div>
+            <div className="company">
+              <div className="title" onClick={() => handleClick(1)}>
+                Company
+                {isActive[1] && (
+                    <span className="mobile-icon">&times;</span> // Cross icon when open (visible in mobile)
+                )}
+                {!isActive[1] && (
+                    <span className="mobile-icon">&#9660;</span> // Right arrow when closed (visible in mobile)
+                )}
+              </div>
+              <div className={isActive[1] ? '' : 'hide'}>
+                <Link to="/home" className="home">Home</Link>
+                <Link to="/aboutus" className="about">About Us</Link>
+                <Link to="/contactus" className="contactus">Contact Us</Link>
+              </div>
+            </div>
+            <div className="contact">
+              <div className="title" onClick={() => handleClick(2)}>
+                Contact
+                {isActive[2] && (
+                    <span className="mobile-icon">&times;</span> // Cross icon when open (visible in mobile)
+                )}
+                {!isActive[2] && (
+                    <span className="mobile-icon">&#9660;</span> // Right arrow when closed (visible in mobile)
+                )}
+              </div>
+              <div className={isActive[2] ? '' : 'hide'}>
+                <p>(+44)749 645 4598</p>
+                <p>twenny@gmail.com</p>
+              </div>
+            </div>
+            <div className="location">
+              <div className="title" onClick={() => handleClick(3)}>
+                Location
+                {isActive[3] && (
+                    <span className="mobile-icon">&times;</span> // Cross icon when open (visible in mobile)
+                )}
+                {!isActive[3] && (
+                    <span className="mobile-icon">&#9660;</span> // Right arrow when closed (visible in mobile)
+                )}
+              </div>
+              <div className={isActive[3] ? '' : 'hide'}>
+                <p>London | Edinburgh</p>
+                <p>Streatham | Newhaven</p>
+                <p>DL3 BDE</p>
+                <p>team@twenny.org</p>
+              </div>
+            </div>
+          </div>
             <div className="footer-bottom">
                 <div className="icons">
                     FB IG IN MAIL TWITTER 
