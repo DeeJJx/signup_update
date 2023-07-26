4baab53710abbcba76811d04917318d6c61be4f4
DanielT94 <daniel.turnbull94@gmail.com>
2023-07-26 15:53:46 +0100

    Started on privacy policy page

--- frontend/src/pages/Privacy.js
+++ frontend/src/pages/Privacy.js
@@ -1,7 +1,67 @@
 const Privacy = () => {
     return (
-        <div>
-            privacy
+        <div className='privacy'>
+            <h2>PRIVACY POLICY</h2>
+
+            <h4>Last updated July 26, 2023</h4>
+            <p>This privacy notice for Twenny Inc ('<b>we</b>', '<b>us</b>' or '<b>our</b>'), describes how we collect, store, use, and/or share ('<b>process</b>') your information when you use our services ('<b>Services</b>'), such as when you:</p>
+            <ul>
+                <li>Visit our website at <a href="http://www.twenny.co.uk">www.twenny.co.uk</a>, or any website of ours that links to this privacy notice</li>
+                <li>Engage with us in other related ways, including and marketing, sales or events</li>
+            </ul>
+            <p><b>Questions or concerns?</b> Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices please do not use our Services. If you still have any questions or concerns, please contact us at twenny@gmail.com</p>
+
+            <h3>SUMMARY OF KEY POINTS</h3> 
+
+            <p><i><b>This summary provides key points from our notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our <a href="table_of_contents">table of contents</a> below to find the section you are looking for.</b></i></p>
+            <p><b>What personal information do we process?</b> When you visit, use, or navigate our Services, we may process personal information depending on how you interact with us and the Services, the choices you make, and the products and features you use. Learn more about <a href="personal_info">personal information you disclose to us.</a></p>
+            <p><b>Do we process any sensitive personal information?</b> We do not process sensitive personal information.</p>
+            <p><b>Do we receive any information from third parties?</b> We do not receive any information from third parties</p>
+            <p><b>How do we process your information?</b> We process you information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have valid legal reason to do so. Learn more about <a href="how_we_process">how we process your information.</a></p>
+            <p><b>In what situations and with which third parties do we share personal information?</b> We may share information in specific situations and with specific third parties. Learn more about <a href="when_we_share">when and with whom we share your personal information.</a></p>
+            <p><b>How do we keep your information safe?</b> We have organisational and technical processes and procedures in place to protect you personal information. However, no electronic transmission over the internet can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorised third parties will not be able to defeat our security and improperly collect, access, steal or modify your information. Learn more about <a href="keep_info_safe">how we keep your information safe.</a></p>
+            <p><b>What are your rights?</b> Depending on where you are located geographically, the applicable privacy laws mean you have certain rights regarding your personal information. Learn more about <a href="privacy_rights">your privacy rights.</a></p>
+            <p><b>How do you exercise your rights?</b> The easiest way to exercise you rights is by submitting a <a href="data_subject_access_request">data subject access request</a>, or by contacting us at twenny@gmail.com. We will consider and act upon any request in accordance with applicable data protection laws.</p>
+            <p>Want to learn more about what we do with any information we collect? Review the privacy notice in full below.</p>
+
+            <h3>TABLE OF CONTENTS</h3>
+            <ol>
+                <li><a href="personal_info">WHAT INFORMATION DO WE COLLECT?</a></li>
+                <li><a href="how_we_process">HOW DO WE PROCESS YOUR INFORMATION?</a></li>
+                <li><a href="legal_bases">WHAT LEGAL BASES DO WE RELY ON TO PROCESS YOUR PERSONAL INFORMATION?</a></li>
+                <li><a href="when_we_share">WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION</a></li>
+                <li><a href="tracking_technologies">DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</a></li>
+                <li><a href="how_long_keep_info">HOW LONG DO WE KEEP YOU INFORMATION?</a></li>
+                <li><a href="keep_info_safe">HOW DO WE KEEP YOUR INFORMATION SAFE?</a></li>
+                <li><a href="minors">DO WE COLLECT INFORMATION FROM MINORS</a></li>
+                <li><a href="privacy_rights">WHAT ARE YOUR PRIVACY RIGHTS?</a></li>
+                <li><a href="do_not_track">CONTROLS FOR DO-NOT-TRACK FEATURES</a></li>
+                <li><a href="Updates_notice">DO WE MAKE UPDATES TO THIS NOTICE?</a></li>
+                <li><a href="contact_notice">HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a></li>
+                <li><a href="review_data">HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</a></li>
+            </ol>
+
+            <h3><a id="personal_info">1. WHAT INFORMATION DO WE COLLECT?</a></h3>
+            <h4>Personal information you disclose to us</h4>
+            <p><b>In Short:</b> we collect personal information that you provide to us.</p>
+            <p>We collect personal information that you voluntarily provide to us when you register on the Services, express an interest in obtaining information about us or our products and Services, when you participate in activities on the Services, or otherwise contact us.</p>
+            <p><b>Personal Information Provided by You.</b> The personal information that we collect depends on the context of your interaction with us and the Services, the choices you make, and the products and features you use. The personal information we collect may include the following:</p>
+            <ul>
+                <li>names</li>
+                <li>phone numbers</li>
+                <li>email addresses</li>
+                <li>mailing addresses</li>
+                <li>job titles</li>
+                <li>usernames</li>
+                <li>passwords</li>
+                <li>contact preferences</li>
+                <li>contact or authentication data</li>
+                <li>billing addresses</li>
+                <li>debit/credit card numbers</li>
+            </ul>
+            <p><b>Sensitive Information.</b> We do not process sensitive information.</p>
+            <p><b>Payment Data.</b> We may collect data necessary to process your payment if you make purchases, such as your payment instrument number, and the security code associated with your payment instrument. All payment data is stored by Stripe. You may find their privacy notice link(s) here: <a href="https://stripe.com/gb/privacy">stripe.com/gb/privacy</a>.</p>
+            <p>All personal information that you provide to us must be true, complete, and accurate, and you must notify us of any changes to such personal information.</p>
         </div>
     )
 }
